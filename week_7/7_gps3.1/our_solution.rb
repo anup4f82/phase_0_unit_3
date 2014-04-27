3eb481435f032727695044ad6842458cf4e96f1a
Anup <anup-pradhan@hotmail.com>
2014-04-27 14:52:07 -0700

    updated user stores,pseudocode and reflections for gps_3.1

--- week_7/7_gps3.1/our_solution.rb
+++ week_7/7_gps3.1/our_solution.rb
@@ -8,8 +8,8 @@
 # As a user, I want to add a item to the Grocery list.
 # As a user, I want to delete an item from the Grocery list
 # As a user, I want to display items in the Grocery list
-# As a user, I want to
-# As a user, I want to
+# As a user, I want to add price and quantity for each item
+
  
 # Pseudocode
 # Define a Class named Grocerylist
@@ -17,7 +17,9 @@
 # Define a Add method that will add items to the cart_list
 #  Define a Delete method that will delete an item from the cart_list
 # Define a display method that will display list of items in the cart_list
-# 
+# Define a Class named Item that will take in arguments as item,quantity and price 
+#  Create a Initialize method for the Item class
+# Create a attr_reader and attr_writer method for item,quantity and price.
  
  
 # Your fabulous code goes here....
@@ -102,6 +104,35 @@ ham = Item.new("Ham",10, 1)
  # struck us that we need to use another class. I initially wasnt sure on how we would code it, but then both my pair and instructor walked me through it 
  # and I understood the code. I felt if I had done this independently I would have thought of the solution much faster. But this is something I need to work on to 
  # collaborate with others and come up with solutions together. Overall I feel pretty confident with creating differnt classes and using objects from one class
- # in another class.
+ # in another class
 
+#  What parts of your strategy worked? What problems did you face?
+
+# The first part of the assignment was fairly simple. But I did have some trouble to understand what needs to be done for the 2nd part
+#where we had to create another class and use the objects from Item class in the grocerylist class. I guess not having the driver code or something to 
+#read was tough for me. But I am very comfortable now with how to use objects from one class in another class.
+
+# What questions did you have while coding? What resources did you find to help you answer them?
+
+# I have done a similar challenge before (creating a playlist), so I knew the concepts.
+
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+
+# I just didnt understand at first what was expected, but I am comfortable now.
+
+# Did you learn any new skills or tricks?
+
+# Just how to use objects from one class in another.
+
+# How confident are you with each of the Learning Competencies?
+
+# Very confident 
+
+# Which parts of the challenge did you enjoy?
+
+# Writing Ruby code is always fun
+
+# Which parts of the challenge did you find tedious?
+
+# None
  
