0dbe522dc1f0c4393ba04ebb79c93a9f50fb65b7
Anup <anup-pradhan@hotmail.com>
2014-05-02 00:31:34 -0700

    adding ruby solutions for fizz_buzz and reverse_words

--- week_8_and_9/4_Ruby/fizz_buzz/my_solution.rb
+++ week_8_and_9/4_Ruby/fizz_buzz/my_solution.rb
@@ -1,17 +1,59 @@
 # U3.W8-9: 
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself
 
 # 2. Pseudocode
+
+# Create a method called super_fizzbuzz that takes in array as argument
+  # create an empty array called result
+  # Loop through the array using each
+   #   if x  % 15 = 0
+    #      add "FizzBuzz" to the result array
+      #      elseif x % 5 = 0
+      #      add "Buzz" to the result array
+
+      #     elseif x % 3  = 0
+      #     add "Fizz" to the result array
+
+      # else add the array element to the result array
+
+  # return result
 
 
 
 # 3. Initial Solution
 
+#  def super_fizzbuzz(array)
+
+# result= []
+# array.each do |x|
+    
+#     if x % 15 == 0
+#         result << "FizzBuzz"
+#         elsif x % 5 == 0
+#         result << "Buzz"
+#         elsif x % 3== 0
+#         result << "Fizz"
+#     else
+#         result << x
+#     end
+    
+# end
+# return result
+# end
+
+
+
+
+
+# 4. Refactored Solution
+
+
  def super_fizzbuzz(array)
 
 result= []
+
 array.each do |x|
     
     if x % 15 == 0
@@ -25,15 +67,8 @@ array.each do |x|
     end
     
 end
-return result
+ result
 end
-
-
-
-
-
-# 4. Refactored Solution
-
 
 
 
@@ -41,9 +76,60 @@ end
 
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
+def random_input_array(base, size)
+  (1..size).map { |i| base**(1+rand(15)) }
+end
+
+def assert
+    
+  raise "Assertion failed" unless yield 
+end
+
+ 
+
+assert {super_fizzbuzz(random_input_array(3,100)) == ["Fizz"]*100}
+assert {super_fizzbuzz(random_input_array(5,100)) == ["Buzz"]*100}
+assert {super_fizzbuzz(random_input_array(15,100)) == ["FizzBuzz"]*100}
+assert {super_fizzbuzz([1,2,3]) == [1,2,'Fizz']}
+assert {super_fizzbuzz([15, 5, 3, 1]) == ['FizzBuzz', 'Buzz', 'Fizz', 1]}
+
+
 
 
 
 
 
 # 5. Reflection 
+
+# What parts of your strategy worked? What problems did you face?
+
+# Having done lot of ruby assignments this was pretty easy
+
+# What questions did you have while coding? What resources did you find to help you answer them?
+
+# None for this exercise
+
+
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+
+# None
+
+
+
+# Did you learn any new skills or tricks?
+
+#No
+
+
+# How confident are you with each of the Learning Competencies?
+
+# Very
+
+# Which parts of the challenge did you enjoy?
+
+# Writing ruby code is always fun
+
+
+# Which parts of the challenge did you find tedious?
+
+# None
--- week_8_and_9/4_Ruby/reverse_words/my_solution.rb
+++ week_8_and_9/4_Ruby/reverse_words/my_solution.rb
@@ -1,26 +1,29 @@
 # U3.W8-9: Reverse Words
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself
 
 # 2. Pseudocode
 
+# Create a method called reverse_words that takes argument as word
+   # Create an array called word by using split method
+   # declare a empty array called 
 
 
 # 3. Initial Solution
-def reverse_words(word)
+# def reverse_words(word)
     
     
     
-   word =  word.split(" ")
-    b = []
-    word.each do |x|
-        y = x.reverse
-        b << y
-    end
- b.join(' ') 
+#    word =  word.split(" ")
+#     b = []
+#     word.each do |x|
+#         y = x.reverse
+#         b << y
+#     end
+#  b.join(' ') 
 
-end
+# end
 
 
 
@@ -29,15 +32,78 @@ end
 # 4. Refactored Solution
 
 
+def reverse_words(word)
+    
+    
+    
+   word =  word.split(" ")
+    result = []
+    word.each do |x|
+        y = x.reverse
+        result << x.reverse
+    end
+ result.join(' ') 
 
+end
 
 
 
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
+def random_word(length = 5)
+  rand(36**length).to_s(36)
+end
+
+def assert
+    
+  raise "Assertion failed" unless yield 
+end
+
+ 
+
+assert {reverse_words("") == "" }
+word = random_word
+assert {reverse_words(word) == word.reverse}
+ word1 = random_word
+  word2 = random_word
+assert {reverse_words("#{word1} #{word2}") == "#{word1.reverse} #{word2.reverse}"}
+assert {reverse_words("Ich bin ein Berliner") == "hcI nib nie renilreB"}
 
 
 
 
 
 # 5. Reflection 
+
+# What parts of your strategy worked? What problems did you face?
+
+# I didnt face any problems for this challenge
+
+# What questions did you have while coding? What resources did you find to help you answer them?
+
+# I had used reverse before, but just had to check and confirm if I can use it.
+
+
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+
+# None
+
+
+
+# Did you learn any new skills or tricks?
+
+#reverse method
+
+
+# How confident are you with each of the Learning Competencies?
+
+# Very
+
+# Which parts of the challenge did you enjoy?
+
+# The rspec passing is satisfying
+
+
+# Which parts of the challenge did you find tedious?
+
+# none
